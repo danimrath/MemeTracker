7c80cc0f2351339ca04a3e4e1ef7e787ece866da
NFriesen12321 <noel.friesen@Norimaconsulting.com>
2022-05-26 14:20:50 -0500

    Person + behavior

---
+++ MemeTracker.DataService/Behavior.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MemeTracker.DataService
+{
+    public class Behavior
+    {
+        public Guid Id { get; set; }
+        public BehaviorType BehaviorType { get; set; }
+        public Guid PersonId { get; set; }
+        public Guid ActivityId { get; set; }
+        public decimal Probability { get; set; }
+    }
+
+    public enum BehaviorType
+    {
+        Create,
+        Share,
+        Read,
+        DoNothing
+    }
+}
---
+++ MemeTracker.DataService/Person.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MemeTracker.DataService
+{
+    public class Person
+    {
+        public Guid Id { get; set; }
+        public List<Person> Friends { get; set; }
+        public List<Behavior> Behaviors { get; set; }
+    }
+}
